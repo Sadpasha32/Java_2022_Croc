3692e9b7f50198a5b17f1756f3ee6ea3bb9e598e
Sadpasha32 <45909626+Sadpasha32@users.noreply.github.com>
2022-10-19 13:23:11 +0300

    Add files via upload

---
+++ src/ru/croc/task2/Task2.java
@@ -0,0 +1,45 @@
+package ru.croc.task2;
+
+public class Task2 {
+    public static Res changeView(Long value){
+        double resVal = value;
+        int numberOfUnit = 0;
+        while(resVal/1024 >= 1){
+            numberOfUnit++;
+            if(numberOfUnit > 4){
+                numberOfUnit = 4;
+                break;
+            }
+            resVal = resVal/1024.0;
+        }
+        return new Res(resVal,numberOfUnit);
+    }
+    public static void main(String[] args){
+        String[] units = {"B","KB","MB","GB","TB"};
+        Res result = changeView(Long.parseLong(args[0]));
+        System.out.println(String.format("%.1f",result.getResVal())+" "+ units[result.getNumberOfUnit()]);
+    }
+}
+class Res{
+    private double resVal;
+    private int numberOfUnit;
+    Res(double resVal, int numberOfUnit){
+        this.resVal = resVal;
+        this.numberOfUnit = numberOfUnit;
+    }
+    public double getResVal() {
+        return resVal;
+    }
+
+    public int getNumberOfUnit() {
+        return numberOfUnit;
+    }
+
+    public void setResVal(double resVal) {
+        this.resVal = resVal;
+    }
+
+    public void setNumberOfUnit(int numberOfUnit) {
+        this.numberOfUnit = numberOfUnit;
+    }
+}
